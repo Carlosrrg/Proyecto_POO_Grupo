0e208d887b44fd399f382c89c474d708780964b8
claudio1995 <claudio100hz@gmail.com>
2017-04-30 00:23:02 -0600

    Problema con editar
    no se por que me tira error  todavia lo demas errores que tiraba cuando
    entraban ya estan

--- wiki_actualizado/WikiLinks/js/manejodiv.js
+++ wiki_actualizado/WikiLinks/js/manejodiv.js
@@ -3,30 +3,65 @@ $(document).ready(function()
 
         $.post("Articulos/Unah.html", function(htmlexterno){
    			$("#articulo").html(htmlexterno);
+        $('#noarticulo').hide();
     	});
 
-    	$('#noarticulo').hide();	
+  
+
+
+
+
 
 });
-
-
 
 function articulo(){
 
 	$.post("Articulos/Unah.html", function(htmlexterno){
    		$("#articulo").html(htmlexterno);
+      $('#noarticulo').hide();
+      $('#articulo').show();
     });
 	
-	$('#noarticulo').hide();
-	$('#articulo').show();
+}
 
+function discusion(){
 
+	$.post("Articulos/Discusion.html", function(htmlexterno){
+   		$("#noarticulo").html(htmlexterno);
+      $('#noarticulo').show();
+      $('#articulo').hide();
+    });
+	
+}
+function leer(){
 
+	$.post("Articulos/Unah.html", function(htmlexterno){
+   		$("#noarticulo").html(htmlexterno);
+      $('#noarticulo').show();
+      $('#articulo').hide();
+    });
+	
+}
+function editar(){
+
+	$.post("EditorTexto/Editor.html", function(htmlexterno){
+   		$("#noarticulo").html(htmlexterno);
+      $('#noarticulo').show();
+      $('#articulo').hide();
+
+    });
+	
+}
+
+function historial(){
+
+	$.post("Articulos/Historial.html", function(htmlexterno){
+   		$("#noarticulo").html(htmlexterno);
+      $('#noarticulo').show();
+      $('#articulo').hide();
+    });
+	
 }
 
 
-function noarticulo(){
-	$('#noarticulo').show();
-	$('#articulo').hide();
 
-}
