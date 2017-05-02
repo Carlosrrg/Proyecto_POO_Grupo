2fcc5c2be3946c16e0cd48ded916ca0058b6e964
midnight25 <tsiki_jlag@yahoo.es>
2017-05-02 02:46:46 -0600

    jose luis
    mmmm aun me faltael login  la parecer no me permite poner mis estilos
    por conflicto con los estilos de la plantilla

--- wiki_actualizado/WikiLinks/php/login.php
+++ wiki_actualizado/WikiLinks/php/login.php
@@ -3,26 +3,23 @@
 <head>
 	<title>WikiLinks</title>
 	<meta charset="UTF-8">
-	<link rel="icon" type="image/png" href="../img/favicon.ico">
-
+	<link rel="icon" type="../image/png" href="../img/favicon.ico" />
 	<link rel="stylesheet" type="text/css" href="../css/style-marco.css">
-
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="../css/bootstrap.min.css">
-	<script src="../js/jquery.min.js"></script>
-	<script src="../js/bootstrap.min.js"></script>
-	<script src="../js/script-marco.js" ></script>
-
+	<link rel="stylesheet" type="text/css" href="../css/estilo-formulario-reportar-articulo.css">
+	
 </head>
 	<body>
-	
-		<br>
-		<div class="div1 container-fluid">
-			<div class="main row">
+		<header>
 
+		</header>
+		<br>
+		<div class="div1 container-fluid ">
+			<div class="main row">
 				<div class="col-xs-12 co-sm-2 col-md-2 col-lg-2 hidden-xs hidden-sm">
-					<img src="../img/wiki.png"width="145" alt="Logo de Wikilinks">
-					<a href="../index.html"><img src="../img/Wikilinkslanding.png" width="145"></a>
+					<img src="../img/wiki.png" width="145" alt="Logo de Wikilinks">
+					<a href="index.html"><img src="../img/Wikilinkslanding.png" width="145"></a>
 					
 					<!--Menu lateral izquierdo-->
 					
@@ -30,20 +27,42 @@
 					  <table class="table table-hover">
 						<tr><td>
 							<ul class="list-group2">
-								<li><a href="../Index.html">Portada</a></li>
-								<li><a href="../php/SubirFoto.php">Subir foto</a></li>
-								<li><a href="../php/reporteArticulo.php">Reportar Articulo</a></li>
-								<li><a href="../php/usuarioModerador.php">Moderador</a></li>
-								<li><a href="../php/subirArticulo.php">Crear paginas nuevas</a></li>
-								<li><a href="">Pagina aleatoria</a></li>
+								<li><a href="#">Portada</a></li>
+								<li><a href="usuarioModerador.php">Moderadores de articulos</a></li>
+								<li><a href="subirArticulo.php">Crear una nueva pagina</a></li>
 								<li><a>Ayuda</a></li>
 								<li><a href="#">Donaciones</a></li>
 								</ul>
 
 						</td></tr>
 						
-						
-						
+						<tr><td>
+							<h6 class="list-left">Imprimir/Exportar</h6>
+							<ul class="list-group1">
+								<li><a>Crear un libro</a></li>
+								<li><a>Version para imprimir</a></li>
+								<a href="javascript:getpdf();">Descargar como PDF</a>
+							</ul>
+						</td></tr>	
+						<tr><td>
+							<h6 class="list-left">En otros proyectos</h6>
+							<ul class="list-group1">
+								<li><a>Wikimedia Commons</a></li>
+							</ul>
+						</td></tr>
+						<tr><td>
+							<h6 class="list-left">Herramientas</h6>
+							<ul class="list-group1">
+								<li><a href="#">Lo que enlaza aqui</a></li>
+								<li><a href="#">Cambios en enlazadas</a></li>
+								<li><a href="#">Subir archivo</a></li>
+								<li><a href="#">Paginas especiales</a></li>
+								<li><a href="#">Enlace permanete</a></li>
+								<li><a>Informacion de la pagina</a></li>
+								<li><a>Elemento de wikidata</a></li>
+								<li><a>Citar esta pagina</a></li>
+							</ul>
+						</td></tr>
 					    </tbody>
 					  </table>
 					</div>
@@ -51,9 +70,9 @@
 					<br>	
 				</div>
 				
-				<div   class=" col-xs-12 col-sm-12 col-md-10 col-lg-10 ">
+				<div  class=" col-xs-12 col-sm-12 col-md-10 col-lg-10 ">
 					<div class="row">
-						<div   class=" col-md-12 col-lg-8 ">
+						<div class="visible-xs visible-sm" >
 							<!--Menu para dispositivos moviles-->
 							<nav class="navbar navbar-default" role="navigation">
 							  <!-- El logotipo y el icono que despliega el menú se agrupan
@@ -77,22 +96,79 @@
 							       otro elemento que se pueda ocultar al minimizar la barra -->
 							  <div class="collapse navbar-collapse navbar-ex1-collapse">
 							    <ul class="nav navbar-nav">
-							      <li class=><a href="../Index.html">Inicio</a></li>
-							      <li><a href="../Principal.html">Aleatorio</a></li>
-							      <li><a href="#">Cercano</a></li>
+							      <li class=><a href="../Index.html">Crear una nueva pagina</a></li>
+							      <li><a href="../Principal.html">Moderadores de Articulos</a></li>
+							      <li><a href="#">Acceder</a></li>
 							    </ul>
 							    <ul class="nav navbar-nav">
-							      <li class=><a href="../php/login.php">Acceder</a></li>
-							      <li><a href="../php/registroUsuario.php">Crear una cuenta</a></li>
-							      <li><a href="../retroAlimentacion.html">Feedback</a></li>
+							      <li class=><a href="../php/login.php">Crear una cuenta</a></li>
+							      <li><a href="../php/registroUsuario.php">Ayuda</a></li>
+							      <li><a href="../Feedback.html">Feedback</a></li>
 							    </ul>
 							 
 							  </div>
 							</nav>
+						</div>
+
+
+						<div class="col-xs-12 col-lg-12 hidden-sm hidden-xs">
+							<div  align="right">
+								<!--Accesos para ventana media, grande-->
+								<h6>
+								<span class="glyphicon glyphicon-user"></span>
+									<a href="login.php">Acceder</a>&nbsp;&nbsp;
+									<a href="registroUsuario.php">Crear una cuenta</a>&nbsp;&nbsp;
+									<a href="../retroAlimentacion.html">Feedback</a>&nbsp;&nbsp;
+
+								</h6>
+							</div>
+						</div>
+						
+						<div class="col-sm-4 col-md-5 hidden-xs hidden-sm">
+							<ul class="nav nav-tabs">
+							    <li id="li-articulo" name="li-articulo" class="">
+							    	<a href="javascript:articulo();">Pagina especial
+							    	</a>
+							    </li>
+							   
+							   
+							</ul>
+						</div>
+						<div class="col-sm-4 col-md-4 col- hidden-xs hidden-sm">
+							<ul class="nav nav-tabs">
+							    <li name="li-leer" id="li-leer" class="">
+							    	<a onClick="loadPages('Articulos/Unah.html',
+							    	'li-leer', 
+							    	'li-articulo',
+							    	'li-editar',
+							    	'li-discusion',
+							    	'li-ver-historial')" href="javascript:noarticulo();">	Leer
+							    	</a>
+							    </li>
+							    <!--li name="li-editar" id="li-editar" class="">
+							    	<a onClick="loadPages('EditorTexto/Editor.html',
+							    	'li-editar', 
+							    	'li-articulo',
+							    	'li-leer',
+							    	'li-discusion',
+							    	'li-ver-historial')" href="javascript:noarticulo();">Editar
+							    	</a>
+							    </li-->
+							    <li name="li-ver-historial" id="li-ver-historial" class="">
+							    	<a onClick="loadPages('Articulos/Historial.html',
+							    	'li-ver-historial', 
+							    	'li-articulo',
+							    	'li-leer',
+							    	'li-editar',
+							    	'li-discusion')" href="javascript:noarticulo();">Ver Historial
+							    	</a>
+							    </li>
+							</ul>
+						</div>
 						
 						
 						<!--Barra de busqueda-->
-						<div class="col-md-10 col-lg-6 col-lg-offset-6 col-md-offset-2">
+						<div class="col-xs-11 col-sm-11 col-md-3">
 							<div class="input-group">
 						      <input type="search" placeholder="Buscar en Wikilinks" class="form-control">
 						      <span class="input-group-btn">
@@ -101,48 +177,42 @@
 								</button>
 						      </span>
 						    </div>
-						</div>
-
-
-						<!--Insertar pagina externa dentro de esta pagina-->						
-						<div class="col-xs-12 col-lg-12">
-							<div class="modal-dialog">
-      							<div class="modal-content">
-                                	<div align="center" class="modal-dialog">
-					                    <img src="../img/logo.png" align="center" width="175px" height="175px">
-					                </div>
-                					<div class="modal-body">
-			                            <form class="col-md-12 center-block">
-			                              <div class="form-group">
-			                                <input type="text"  class="form-control input-lg" placeholder="Usuario" autofocus>
-			                              </div>
-			                              <div class="form-group">
-			                                    <input type="password" class="form-control input-lg" placeholder="Contraseña">
-			                              </div>
-			                             <div class="form-group">
-			                                <input type="submit" class="btn btn-block btn-lg btn-primary" value="Ingresar"><br>
-			                                <span class="pull-right"><a href="../php/registroUsuario.php">Registrarse</a></span>
-			                                <span><a href="#">¿Olvidó su contraseña?</a></span>
-			                              </div>
-			                            </form>
-                 	 					
-                 	 					<div class="modal-footer">
-                    						<div>
-                     					 		<button class="btn">Cancelar</button>
-                    						</div>
-                 						 </div>
-       								 </div>        
-     							 </div>
-     						</div>
-						</div>
-					</div>
+						</div><br><br><br><br>
+						<!--registro-->
+								<h1>Ingresar</h1>
+								<span>necesitas tener cookies permisos para poder registrarte en wikilinks </span>
+								<form style="width: 400px; padding-left: px;">
+										  <div class="form-group">
+											    <label for="text">nombre</label>
+											    <input type="text" class="form-control" id="txt-nombre">
+										  </div>
+										  <div class="form-group">
+											    <label for="pwd">Contrasena:</label>
+											    <input type="password" class="form-control" id="txt-contrasena">
+										  </div>
+										  <div class="checkbox">
+										    <label><input type="checkbox" align="left"> mantener mi sesion iniciada </label>
+										  </div>
+										  <button type="submit" class="btn btn-primary">acceder</button>
+								</form>
+													
+													
 					
 				</div>
+				
+					
 				</div>
 			</div>
-		<br><br><br>
-		<footer >-Programacion Orientada a Objetos 1500-</footer>
-		<br>
+			<br><br><br>
+			<footer >©CopyRigth 2017 Proyecto de POO</footer>
+			<br>
 		</div>
+		<script src="../js/jquery.min.js"></script>
+		<script src="../js/bootstrap.min.js"></script>
+		<script src="../js/script-marco.js"></script>
+		<script type="text/javascript" src="../js/manejodiv.js"></script>
+		<script type="text/javascript" src="../js/jquery-2.1.4.min.js"></script>
+		<script type="text/javascript" src="../js/descargarpdf.js"></script>
+		<script type="text/javascript" src="../js/jspdf.min.js"></script>
 	</body>
 </html>
--- wiki_actualizado/WikiLinks/php/registroUsuario.php
+++ wiki_actualizado/WikiLinks/php/registroUsuario.php
@@ -1,52 +1,69 @@
 <!DOCTYPE html>
 <html>
 <head>
+
 	<title>WikiLinks</title>
 	<meta charset="UTF-8">
-	<link rel="icon" type="image/png" href="../img/favicon.ico">
-
+	<link rel="icon" type="../image/png" href="../img/favicon.ico" />
 	<link rel="stylesheet" type="text/css" href="../css/style-marco.css">
-
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="../css/bootstrap.min.css">
-	<script src="../js/jquery.min.js"></script>
-	<script src="../js/bootstrap.min.js"></script>
-	<script src="../js/script-marco.js" ></script>
-
+	<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="../css/estilo-formulario-reportar-articulo.css">
+	
 </head>
 	<body>
 		<header>
-	
 
 		</header>
 		<br>
-		<div class="div1 container-fluid">
+		<div class="div1 container-fluid ">
 			<div class="main row">
-
 				<div class="col-xs-12 co-sm-2 col-md-2 col-lg-2 hidden-xs hidden-sm">
-					<img src="../img/wiki.png"width="145" alt="Logo de Wikilinks">
-					<a href="../index.html"><img src="../img/Wikilinkslanding.png" width="145"></a>
+					<img src="../img/wiki.png" width="145" alt="Logo de Wikilinks">
+					<a href="index.html"><img src="../img/Wikilinkslanding.png" width="145"></a>
 					
 					<!--Menu lateral izquierdo-->
 					
-					<div class="">            
+					<div >            
 					  <table class="table table-hover">
 						<tr><td>
 							<ul class="list-group2">
-								<li><a href="../Index.html">Portada</a></li>
-								<li><a href="../php/SubirFoto.php">Subir foto</a></li>
-								<li><a href="../php/reporteArticulo.php">Reportar Articulo</a></li>
-								<li><a href="../php/usuarioModerador.php">Moderador</a></li>
-								<li><a href="../php/subirArticulo.php">Crear paginas nuevas</a></li>
-								<li><a href="">Pagina aleatoria</a></li>
+								<li><a href="#">Portada</a></li>
+								<li><a href="usuarioModerador.php">Moderadores de articulos</a></li>
+								<li><a href="subirArticulo.php">Crear una nueva pagina</a></li>
 								<li><a>Ayuda</a></li>
 								<li><a href="#">Donaciones</a></li>
 								</ul>
 
 						</td></tr>
 						
-						
-						
+						<tr><td>
+							<h6 class="list-left">Imprimir/Exportar</h6>
+							<ul class="list-group1">
+								<li><a>Crear un libro</a></li>
+								<li><a>Version para imprimir</a></li>
+								<a href="javascript:getpdf();">Descargar como PDF</a>
+							</ul>
+						</td></tr>	
+						<tr><td>
+							<h6 class="list-left">En otros proyectos</h6>
+							<ul class="list-group1">
+								<li><a>Wikimedia Commons</a></li>
+							</ul>
+						</td></tr>
+						<tr><td>
+							<h6 class="list-left">Herramientas</h6>
+							<ul class="list-group1">
+								<li><a href="#">Lo que enlaza aqui</a></li>
+								<li><a href="#">Cambios en enlazadas</a></li>
+								<li><a href="#">Subir archivo</a></li>
+								<li><a href="#">Paginas especiales</a></li>
+								<li><a href="#">Enlace permanete</a></li>
+								<li><a>Informacion de la pagina</a></li>
+								<li><a>Elemento de wikidata</a></li>
+								<li><a>Citar esta pagina</a></li>
+							</ul>
+						</td></tr>
 					    </tbody>
 					  </table>
 					</div>
@@ -54,9 +71,9 @@
 					<br>	
 				</div>
 				
-				<div   class=" col-xs-12 col-sm-12 col-md-10 col-lg-10 ">
+				<div  class=" col-xs-12 col-sm-12 col-md-10 col-lg-10 ">
 					<div class="row">
-						<div   class=" col-md-12 col-lg-8 ">
+						<div class="visible-xs visible-sm" >
 							<!--Menu para dispositivos moviles-->
 							<nav class="navbar navbar-default" role="navigation">
 							  <!-- El logotipo y el icono que despliega el menú se agrupan
@@ -80,22 +97,79 @@
 							       otro elemento que se pueda ocultar al minimizar la barra -->
 							  <div class="collapse navbar-collapse navbar-ex1-collapse">
 							    <ul class="nav navbar-nav">
-							      <li class=><a href="../Index.html">Inicio</a></li>
-							      <li><a href="../Principal.html">Aleatorio</a></li>
-							      <li><a href="#">Cercano</a></li>
+							      <li class=><a href="../Index.html">Crear una nueva pagina</a></li>
+							      <li><a href="../Principal.html">Moderadores de Articulos</a></li>
+							      <li><a href="#">Acceder</a></li>
 							    </ul>
 							    <ul class="nav navbar-nav">
-							      <li class=><a href="../php/login.php">Acceder</a></li>
-							      <li><a href="../php/registroUsuario.php">Crear una cuenta</a></li>
-							      <li><a href="../retroAlimentacion.html">Feedback</a></li>
+							      <li class=><a href="../php/login.php">Crear una cuenta</a></li>
+							      <li><a href="../php/registroUsuario.php">Ayuda</a></li>
+							      <li><a href="../Feedback.html">Feedback</a></li>
 							    </ul>
 							 
 							  </div>
 							</nav>
+						</div>
+
+
+						<div class="col-xs-12 col-lg-12 hidden-sm hidden-xs">
+							<div  align="right">
+								<!--Accesos para ventana media, grande-->
+								<h6>
+								<span class="glyphicon glyphicon-user"></span>
+									<a href="login.php">Acceder</a>&nbsp;&nbsp;
+									<a href="registroUsuario.php">Crear una cuenta</a>&nbsp;&nbsp;
+									<a href="../retroAlimentacion.html">Feedback</a>&nbsp;&nbsp;
+
+								</h6>
+							</div>
+						</div>
+						
+						<div class="col-sm-4 col-md-5 hidden-xs hidden-sm">
+							<ul class="nav nav-tabs">
+							    <li id="li-articulo" name="li-articulo" class="">
+							    	<a href="javascript:articulo();">	Pagina especial
+							    	</a>
+							    </li>
+							
+							   
+							</ul>
+						</div>
+						<div class="col-sm-4 col-md-4 col- hidden-xs hidden-sm">
+							<ul class="nav nav-tabs">
+							    <li name="li-leer" id="li-leer" class="">
+							    	<a onClick="loadPages('Articulos/Unah.html',
+							    	'li-leer', 
+							    	'li-articulo',
+							    	'li-editar',
+							    	'li-discusion',
+							    	'li-ver-historial')" href="javascript:noarticulo();">	Leer
+							    	</a>
+							    </li>
+							    <!--li name="li-editar" id="li-editar" class="">
+							    	<a onClick="loadPages('EditorTexto/Editor.html',
+							    	'li-editar', 
+							    	'li-articulo',
+							    	'li-leer',
+							    	'li-discusion',
+							    	'li-ver-historial')" href="javascript:noarticulo();">Editar
+							    	</a>
+							    </li-->
+							    <li name="li-ver-historial" id="li-ver-historial" class="">
+							    	<a onClick="loadPages('Articulos/Historial.html',
+							    	'li-ver-historial', 
+							    	'li-articulo',
+							    	'li-leer',
+							    	'li-editar',
+							    	'li-discusion')" href="javascript:noarticulo();">Ver Historial
+							    	</a>
+							    </li>
+							</ul>
+						</div>
 						
 						
 						<!--Barra de busqueda-->
-						<div class="col-md-10 col-lg-6 col-lg-offset-6 col-md-offset-2">
+						<div  class="col-xs-11 col-sm-11 col-md-3">
 							<div class="input-group">
 						      <input type="search" placeholder="Buscar en Wikilinks" class="form-control">
 						      <span class="input-group-btn">
@@ -104,66 +178,64 @@
 								</button>
 						      </span>
 						    </div>
+						</div><br><br><br><br>
+						
+
+					</div >
+					<!--Registro usuarios -->
+					<div><h1>Crear una cuenta</h1></div>
+						
+					<div  class="row" style=" float: left; position: relative; 50%">
+						
+						
+
+						<div class="form-group" style="width: 80%" >
+							Nombre
+							<input type="text"    placeholder="Escriba un nombre de usuario" name="txt-nombre" autofocus required>
+						</div>
+						<div class="form-group">
+							Contrasena
+							<input type="password" style="width: 80%" name="contrasena" placeholder="Escriba una contrasena" required>
+						</div>
+						<div>
+							Confirmar contrasena
+							<input type="password" style="width: 80%" name="confirmar-contrasena" placeholder="Repita la contrasena" required>
+						</div>
+						<div>
+							Correo electronico (opcional)
+							<input type="email" name="correo" style="width: 80%" value="Escriba una direcion de Correo">
 						</div>
 
-						<!--Insertar pagina externa dentro de esta pagina-->						
-						<div class="col-xs-12 col-lg-12">
-							<div class="col-md-6 portfolio-item"> 
-							<h1>Crea Tu Cuenta!</h1>
-						        <form id="registroUser" action="">
-					           		<table class="table table-hover">
-									   	<tr>
-									    	<td>Usuario:</td>
-									        <td><input id="txt-nombre" name="txt-nombre" placeholder="Ingresa un nombre de usuario" type="text" class="form-control"></td>
-									    </tr>
-									    <tr>
-									    	<td>Contrasena:</td>
-									        <td><input id="txt-contrasena" name="txt-contrasena" placeholder="Escribe una contrasena" type="password" class="form-control"></td>
-									    </tr>
-									    <tr>
-									    	<td>Repite Contrasena</td>
-									        <td><input id="txt-confirmar-contrasena" name="txt-confirmar-contrasena" placeholder="Repite la contrasena" type="password" class="form-control"></td>
-									    </tr>
-									    <tr>
-									    	<td>Direccion de correo</td>
-									        <td><input id="txt-correo" name="txt-correo" placeholder="Ingresa un correo(opcional)" type="email" class="form-control"></td>
-									    </tr>
-									    <tr>
-									        <td colspan="2">
-									            <center>
-									            <button type="button" class="btn btn-primary" id="btn-registrar"   name="btn-registrar" style="background-color: #447FF5;" data-loading-text="Guardando..." >Registrarme</button>
-
-									           </center>
-									        </td>
-									    </tr>
-						      		</table>
-						      	</form> 
-						      	<div id="resultado" name="resultado">hola</div>  
-						    </div>
-							
-
-
-							<div class="col-md-6 portfolio-item"> 
-								<center><h2>Personas como vos son las que construyen Wikilinks!</h2>
+							<button style="width: 80%" class="btn btn-primary" alt="registrarse">Crear una cuenta</button>
+						
+					</div>
+					<div style=" width: 50%;  float: left; margin-left: 20px;" class="col-xs-11 ">
+						<center>
+								<h2>Personas como tú son las que construyen wikilinks.!</h2>
 				  				<h3>987,5464,8466</h3>
 				  				<label>ediciones</label>
 				  				<h3>5,646,546</h3>
 				  				<label>paginas</label>
 				  				<h3>335,156</h3>
 				  				<label>colaboradores recientes</label>
-				  				</center>
-						    </div>
-						</div>
-					</div>	
+				  		<center>
+				  				
+					</div>
+
+				
 				</div>
-				</div>
+
 			</div>
-		<br><br><br>
-		<footer >-Programacion Orientada a Objetos 1500-</footer>
-		<br>
+			<br><br><br>
+			<footer >©CopyRigth 2017 Proyecto de POO</footer>
+			<br>
 		</div>
-		
-		
-		<script type="text/javascript" src="js/registro_usuarios.js"></script>
+		<script src="../js/jquery.min.js"></script>
+		<script src="../js/bootstrap.min.js"></script>
+		<script src="../js/script-marco.js"></script>
+		<script type="text/javascript" src="../js/manejodiv.js"></script>
+		<script type="text/javascript" src="../js/jquery-2.1.4.min.js"></script>
+		<script type="text/javascript" src="../js/descargarpdf.js"></script>
+		<script type="text/javascript" src="../js/jspdf.min.js"></script>
 	</body>
 </html>
