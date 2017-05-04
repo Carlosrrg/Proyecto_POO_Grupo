a9c16a25dbde0a169f8bceaebf8e294d15550add
midnight25 <tsiki_jlag@yahoo.es>
2017-05-03 22:02:53 -0600

    re

---
+++ wiki_actualizado/WikiLinks/js/controladorRegistro.js
@@ -0,0 +1,32 @@
+	//procesa el formulario de registro de usuario
+$(document).ready(function(){
+
+	$("#ingresar").click(function(){
+		var parametros=
+				"txt-nombre="+$("#txt-nombre").val()+"&"+
+				"txt-correo="+$("#correo").val()+"&"+
+				"txt-contrasena"+$("#contrasena").val()+"&"+
+				"txt-confirmar-contrasena="+$("#confirmar-contrasena").val();
+	            //alert("parametros a enviar " + parametros);
+				$.ajax({
+						url:"../ajax/acciones_login.php?accion=2",
+						data:parametros,
+						method:"POST",
+						dataType: "json",
+							success:function(respuesta){
+								if (respuesta["codigo"]==1) {
+										alert(respuesta["mensaje"]);
+								}else{
+									alert(respuesta["mensaje"]);
+								}
+								
+								
+								
+							}
+						
+				});
+
+	});
+	
+
+});
