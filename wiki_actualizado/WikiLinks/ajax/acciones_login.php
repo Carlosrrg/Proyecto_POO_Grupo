b6a069decb03dce3d6bb8a0f802ffce5c7c634c7
midnight25 <tsiki_jlag@yahoo.es>
2017-05-03 22:16:39 -0600

    cometario

--- wiki_actualizado/WikiLinks/ajax/acciones_login.php
+++ wiki_actualizado/WikiLinks/ajax/acciones_login.php
@@ -14,6 +14,7 @@
 			echo json_encode($respuesta);
 			break;
 		case '2':
+		//crea un usuario y llama al metodo guardar regristo que recie una conexion como para mettro
 			include_once("../class/class_conexion.php");
 			include_once("../class/class_usuario");
 			//primero comprueba si las contrasenas coinciden
