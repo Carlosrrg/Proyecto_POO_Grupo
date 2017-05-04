f835b03b0fcec8723d8b63ad52bdaa262a2a1fd0
midnight25 <tsiki_jlag@yahoo.es>
2017-05-04 11:58:24 -0600

    a

--- wiki_actualizado/WikiLinks/ajax/acciones_login.php
+++ wiki_actualizado/WikiLinks/ajax/acciones_login.php
@@ -7,14 +7,14 @@
 			include_once("../class/class_conexion.php");
 			include_once("../class/class_usuario.php");
 			$conexion = new Conexion();
-			$respuesta = Usuario::verificarUsuario($conexion,$_POST["inputEmail"],$_POST["inputPassword"]);
+			$respuesta = Usuario::verificarUsuario($conexion,$_POST["txt-nombre"],$_POST["txt-contrasena"]);
 			$_SESSION["codigo_usuario"] = $respuesta["codigo_usuario"];
 			$_SESSION["nombre_usuario"] = $respuesta["nombre_usuario"];
 			$_SESSION["codigo_tipo_usuario"] = $respuesta["codigo_tipo_usuario"];
 			echo json_encode($respuesta);
 			break;
 		case '2':
-		//crea un usuario y llama al metodo guardar regristo que recie una conexion como parametros
+		//crea un usuario y llama al metodo guardar regristo que recie una conexion como para mettro
 			include_once("../class/class_conexion.php");
 			include_once("../class/class_usuario");
 			//primero comprueba si las contrasenas coinciden
