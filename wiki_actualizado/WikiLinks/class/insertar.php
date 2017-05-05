297c2b84f667679dc9bb4b75f1eb63ceae5e097c
ikserm <iksermd@gmail.com>
2017-05-05 03:51:56 -0600

    jh

--- wiki_actualizado/WikiLinks/class/insertar.php
+++ wiki_actualizado/WikiLinks/class/insertar.php
@@ -2,8 +2,8 @@
 	include_once(class_conexion.php);
 	if(isset($_POST['nombre-articulo']) && !empty($_POST['nombre-articulo']) && isset($_POST['area-editor']) && !empty($_POST['area-editor']) ){
 
-		mysql_connect($host, $user, area-editor
-			)
+		$con=mysql_connect($host, $user, $area-editor)or die("Problemas al conectar");
+		mysql_select_db($db,$con)or die("Problemas al conectar la bd");
 	}
 
 ?>
